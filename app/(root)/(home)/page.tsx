e29c8ee817a438d08f90101d6a1fb8c38d17921b
divyanshuelred <divyanshu@elred.io>
2024-02-23 11:19:29 +0530

    Homepage UI

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,3 +1,99 @@
+import QuestionCard from "@/components/cards/QuestionCard";
+import HomeFilters from "@/components/home/HomeFilters";
+import Filter from "@/components/shared/Filter";
+import NoResult from "@/components/shared/NoResult";
+import LocalSearchBar from "@/components/shared/search/LocalSearchBar";
+import { Button } from "@/components/ui/button";
+import { HomePageFilters } from "@/constants/filters";
+import Link from "next/link";
+
+const questions = [
+  {
+    _id: "1",
+    title: "cascading syle sheets is called CSS in short",
+    tags: [
+      { _id: "1", name: "python" },
+      { _id: "2", name: "javascript" },
+    ],
+    author: {
+      _id: "1",
+      name: "Divyanshu",
+      picture: "assets/icons/search.svg",
+    },
+    upvotes: 10000,
+    answers: [],
+    views: 1000000,
+    createdAt: new Date("2023-10-10T12:00:00Z"),
+  },
+  {
+    _id: "2",
+    title: "How to center a div?",
+    tags: [
+      { _id: "3", name: "html" },
+      { _id: "4", name: "css" },
+    ],
+    author: {
+      _id: "2",
+      name: "John Doe",
+      picture: "assets/icons/search.svg",
+    },
+    upvotes: 15,
+    answers: [],
+    views: 150,
+    createdAt: new Date("2024-01-11T08:00:00Z"),
+  },
+];
+
 export default function Home() {
-  return <div className="h-screen"></div>;
+  return (
+    <>
+      <div className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
+        <h1 className="h1-bold text-dark100_light900">All Questions</h1>
+        <Link href="/ask-questions" className="flex justify-end max-sm:w-full">
+          <Button className="primary-gradient min-h-[46px] px-4 py-3 !text-light-900">
+            Ask Question
+          </Button>
+        </Link>
+      </div>
+      <div className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
+        <LocalSearchBar
+          route="/"
+          iconPosition="left"
+          imgSrc="assets/icons/search.svg"
+          placeholder="Search for questions..."
+          otherClasses="flex-1"
+        />
+        <Filter
+          filters={HomePageFilters}
+          otherClasses="min-h-[56px] sm:min-w-[170px]"
+          containerClasses="hidden max-md:flex"
+        />
+      </div>
+      <HomeFilters />
+      <div className="mt-10 flex w-full flex-col gap-6">
+        {questions.length > 0 ? (
+          questions.map((question) => (
+            <QuestionCard
+              key={question._id}
+              _id={question._id}
+              title={question.title}
+              tags={question.tags}
+              author={question.author}
+              upvotes={question.upvotes}
+              answers={question.answers}
+              views={question.views}
+              createdAt={question.createdAt}
+            />
+          ))
+        ) : (
+          <NoResult
+            title="There's no questions to show"
+            description="Be the first to break the silence! 🚀 Ask a Question and kickstart the discussion. our query could be the next big thing others learn from. Get involved! 💡"
+            link="/ask-questions"
+            linkTitle="Ask a question"
+          />
+        )}
+      </div>
+    </>
+  );
 }
