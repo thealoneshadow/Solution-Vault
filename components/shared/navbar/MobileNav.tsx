eebd4b96a57b8133b5b1bd3ad0a5712233c1b97b
divyanshuelred <divyanshu@elred.io>
2024-02-26 22:32:45 +0530

    New Logo

--- components/shared/navbar/MobileNav.tsx
+++ components/shared/navbar/MobileNav.tsx
@@ -65,7 +65,7 @@ const MobileNav = () => {
       >
         <Link href="/" className="flex items-center gap-1">
           <Image
-            src="/assets/images/site-logo.svg"
+            src="/assets/images/site-logo2.png"
             width={23}
             height={23}
             alt="Solution Vault"
