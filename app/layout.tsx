2606c9d2d5610f019d048ce9e33a5e4f32052948
divyanshuelred <divyanshu@elred.io>
2024-02-26 12:40:10 +0530

    Name Fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ const spaceGrotesk = Space_Grotesk({
 });
 
 export const metadata: Metadata = {
-  title: "DevOverflow",
+  title: "Solution Valut",
   description:
     "A community-driven platform for asking and answering questions about software development. Get help, share knowledge, and collaborate with developers from around the world. Explore topics in web development, mobile app development, game development, algorithms, data structures, and more.",
   icons: {
