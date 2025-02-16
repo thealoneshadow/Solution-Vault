7522d03934a7f1edc67df67d2a543474687c08fe
Divyanshu Goyal <divyanshugoyal@Divyanshus-MacBook-Pro.local>
2025-02-16 15:13:19 +0530

    Bug Fixes

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -52,7 +52,7 @@ export default async function Home() {
           ))
         ) : (
           <NoResult
-            title="There's no questions to show"
+            title="There's no question to show"
             description="Be the first to break the silence! 🚀 Ask a Question and kickstart the discussion. our query could be the next big thing others learn from. Get involved! 💡"
             link="/ask-question"
             linkTitle="Ask a question"
